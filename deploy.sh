9fa4ed5ad65ceca6e12531606d7c231eba5ab6a8
raju8309 <rajukotturi45@gmail.com>
2025-05-04 14:16:00 -0400

    Updates

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm run build
-echo "Build complete! Open https://app.netlify.com/drop and drag the 'dist' folder there for instant deployment."
-open https://app.netlify.com/drop
-open dist
